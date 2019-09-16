138e281d65299103837f05e0b381e1433e9bf7b3
Ryan Lewis <ryan.lewis@anl.gov>
2019-09-16 16:04:02 +0000

    Multi-node processing with process pool up and running

--- cutil-example/submit.sh
+++ cutil-example/submit.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
-#COBALT -t 60
-#COBALT -n 1
+#COBALT -t 120
+#COBALT -n 2
 #COBALT -q debug
 #COBALT -O ./output/$jobid
 #COBALT -M ryan.lewis@anl.gov
+#COBALT -A AMASE
 
-year=2016
-month=1
-day=1
+year=$1
+month=$2
 
-for file in /gpfs/mira-fs0/logs/darshan/mira/$year/$month/$day/*
+NODES=`cat $COBALT_NODEFILE`
+NODE_ARRAY=($NODES)
+
+# day=2
+# let "index = ${day} - 1"
+# ssh ${NODE_ARRAY[$index]} pwd
+
+for day in {1..2} #{1..31}
 do
-	./process-log $file ../data/darshan/${year}_${month}_${day}_$(basename $file).csv
+	if [ $day == 1 ]; then
+		/home/rlewis/repos/jupyter-notebooks/cutil-example/process.sh $year $month $day &
+	else
+		let "index = ${day} - 1"
+		ssh ${NODE_ARRAY[$index]} "/home/rlewis/repos/jupyter-notebooks/cutil-example/process.sh $year $month $day" &
+	fi
 done
+wait
